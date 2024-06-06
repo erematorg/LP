95c1380f12a31a202de809fb583983e4a7769c0c
Karto1234 <nckarthik1@gmail.com>
2024-06-06 20:07:44 +1000

    Added comments

--- LP/systems/ai/behaviour_tree/nodes/composite/Selector.cs
+++ LP/systems/ai/behaviour_tree/nodes/composite/Selector.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 
 [GlobalClass]
-public partial class Selector : BTComposite
+public partial class Selector : BTComposite //Runs each child node in order until one succeeds. Returns Success if one succeeds, returns Failure if all children fail.
 {
     public override BTResult Tick(Entity entity, Blackboard bb)
     {
--- LP/systems/ai/behaviour_tree/nodes/composite/Sequencer.cs
+++ LP/systems/ai/behaviour_tree/nodes/composite/Sequencer.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 
 [GlobalClass]
-public partial class Sequencer : BTComposite
+public partial class Sequencer : BTComposite //Runs each child node in order until one fails. Returns Failure if one fails, returns Success if all children succeed.
 {
     public override BTResult Tick(Entity entity, Blackboard bb)
     {
