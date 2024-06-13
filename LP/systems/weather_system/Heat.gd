db98846ccd6e4760adbee17b0b668eb14b5ab1b7
Andres David Calderon <david.voidgames@gmail.com>
2024-06-13 10:53:38 -0500

    basic code for moisture management
    Next we need moisture to move to colder air, so that it can become rain

---
+++ LP/systems/weather_system/Heat.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name Temperature
+
+## Used before taking into account altitude
+@export var default_temperature:float
+
+## Higher air is less dense, and as such colder.
+## This controls how much base temperature changes per unit of altitude.
+@export var altitude_temperature_change:float
+var temperature_per_area:Dictionary
+
+func get_temperature(area:Vector2i):
+	if not temperature_per_area.has(area):
+		temperature_per_area[area]=default_temperature
+		temperature_per_area[area]-=altitude_temperature_change*area.y
+	return temperature_per_area[area]
--- LP/systems/weather_system/Humidity.gd
+++ LP/systems/weather_system/Humidity.gd
@@ -1,8 +1,17 @@
 extends Node
 
 @export var water_evaporation_per_tick:float
+## Default amount of moisture the air can hold, in px squared.
+## Gets higher with temperature, exactly this value when temperature is
+## Temperature.default_temperature
+@export var max_air_humidity:float
+
+## How much more moisture can air hold per higher degree of temperature
+@export var max_humidity_change:float
 
 var air_humidity_per_area:Dictionary
+
+@onready var temperature: Temperature = get_node("%Temperature")
 
 func get_air_humidity(area:Vector2i):
 	if air_humidity_per_area.has(area):
@@ -10,14 +19,20 @@ func get_air_humidity(area:Vector2i):
 	else:
 		return 0
 
+func get_max_humidity(area:Vector2i):
+	return max_air_humidity+(max_humidity_change*(temperature.get_temperature(area)-temperature.default_temperature))
+
 func absorb_water():
 	for i in get_tree().get_nodes_in_group("water_puddles"):
 		for area in i.covered_areas:
 			if not air_humidity_per_area.has(area):
 				air_humidity_per_area[area]=0
-			air_humidity_per_area[area]+=water_evaporation_per_tick
-			i.reduce(water_evaporation_per_tick)
+			if air_humidity_per_area[area]<max_air_humidity:
+				air_humidity_per_area[area]+=water_evaporation_per_tick
+				i.reduce(water_evaporation_per_tick)
 
+func distribute_humidity():
+	pass
 
 func _on_tick_timeout():
 	absorb_water()
