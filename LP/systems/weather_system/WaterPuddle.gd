ad9a1f6f23581a159c940ec60052d76416336d6b
Andres David Calderon <david.voidgames@gmail.com>
2024-06-12 22:41:04 -0500

    the simpler solution

--- LP/systems/weather_system/WaterPuddle.gd
+++ LP/systems/weather_system/WaterPuddle.gd
@@ -14,86 +14,60 @@ func reduce(area:float):
 	if is_queued_for_deletion():
 		return
 	
+	ensure_flat_surface()
+	
 	var surface_points : Array=get_surface_points()
 	var surface_point_indexes: Array[int]=[]
 	for i in surface_points:
 		var index=polygon.find(i)
 		surface_point_indexes.append(index)
-	# Ensure the top is flat, as real water
-	var new_polygon=polygon
-	new_polygon[surface_point_indexes[0]].y=new_polygon[surface_point_indexes[1]].y
-	polygon=new_polygon
-	surface_points[0].y=surface_points[1].y
-
+	
+	# Determine height to remove
 	var width=WeatherUtilities.num_distance(surface_points[0].x,surface_points[1].x)
 	var height_to_remove=area/width
-	
-	if height_to_remove>get_height()+20:
+	if height_to_remove<get_height()+20:
+		var cut_height = surface_points[0].y - height_to_remove
+		var x_ordered_polygon: Array = Array(polygon)
+		x_ordered_polygon.sort_custom(is_rightmost_x)
+		var polyline=Geometry2D.offset_polyline([Vector2(x_ordered_polygon[0].x-1,cut_height),Vector2(x_ordered_polygon[polygon.size()-1].x+1,cut_height)],10)
+		var new_polygons=Geometry2D.clip_polygons(
+			polygon,
+			polyline[0]
+		)
+		
+		if new_polygons.is_empty():
+			queue_free()
+			return 
+		
+		polygon=new_polygons[0]
+		var index=1
+		while index<new_polygons.size():
+			var new_body=Puddle.new()
+			var new_polygon=new_polygons[index]
+			for i in range(new_polygon.size()):
+				new_polygon[i] = new_polygon[i]+position
+			new_body.polygon=PackedVector2Array(new_polygon)
+			new_body.color=Color.BLUE
+			get_parent().add_child(new_body)
+			new_body.call_deferred("add_to_group","water_puddles")
+			index+=1
+	else:
 		queue_free()
-		return
+
+##Ensure the top 2 points are in the same height
+func ensure_flat_surface():
+	# Ensure the top is flat, as real water
+	var new_polygon=polygon
+	var surface_points=get_surface_points()
+	new_polygon[polygon.find(surface_points[0])].y=new_polygon[polygon.find(surface_points[1])].y
+	polygon=new_polygon
 	
-	for i in surface_point_indexes:
-		polygon[i].y+=height_to_remove
-	
-	var new_surface_point=get_surface_points()
-	# We have to rely on index to check when the surface points get surpassed
-	# because we already lowered said points.
-	var new_surface_point_indexes=[polygon.find(new_surface_point[0]),polygon.find(new_surface_point[1])]
-	if (not new_surface_point_indexes.has(surface_point_indexes[0])) or (not new_surface_point_indexes.has(surface_point_indexes[1])):
-		divide(surface_point_indexes)
 
 func get_height():
 	var points=Array(polygon)
 	points.sort_custom(is_higher_y)
 	return WeatherUtilities.num_distance(points[0].y,points[1].y)
 
-## Divides this puddle into smaller puddles that do not exceed the height of the current one.
-func divide(last_surface_point_indexes:Array[int]):
-	var last_surface_points: Array[Vector2]=[polygon[last_surface_point_indexes[0]],polygon[last_surface_point_indexes[1]]]
-	#Indexes of points above last surface points
-	var cutting_indexes:Array[int]=[]
-	for index in range(polygon.size()):
-		var point=polygon[index]
-		if point.y<=last_surface_points[0].y and not last_surface_points.has(point):
-			cutting_indexes.append(index)
-	var index=last_surface_point_indexes.max()
-	var new_polygons: Array[Array]=[[polygon[index]]]
-	var polygon_index=0
-	while true:
-		index+=1
-		if last_surface_point_indexes.has(index):
-			new_polygons[polygon_index].append(polygon[index])
-			break
-		if index==polygon.size():
-			index=0
-		var current_polygon=new_polygons[polygon_index]
-		if cutting_indexes.has(index):
-			if current_polygon.size()<2:
-				current_polygon.clear()
-				continue
-			
-			var last_point:Vector2 = current_polygon[current_polygon.size()-1]
-			##Ads the cut between the surface and the "island" point.
-			current_polygon.append(Geometry2D.line_intersects_line(
-					last_point,
-					last_point.direction_to(polygon[index]),
-					last_surface_points[0],
-					last_surface_points[0].direction_to(last_surface_points[1])
-			))
-			polygon_index+=1
-			new_polygons.append([])
-			continue
-		new_polygons[polygon_index].append(polygon[index])
-	
-	for new_polygon in new_polygons:
-		var new_body=Puddle.new()
-		for i in range(new_polygon.size()):
-			new_polygon[i] = new_polygon[i]+position
-		new_body.polygon=PackedVector2Array(new_polygon)
-		new_body.color=Color.BLUE
-		get_parent().add_child(new_body)
-		new_body.call_deferred("add_to_group","water_puddles")
-	queue_free()
 ## Returns the 2 higher points of the puddle
 func get_surface_points():
 	var points=Array(polygon)
@@ -103,4 +77,6 @@ func get_surface_points():
 
 func is_higher_y(a,b):
 	return a.y<b.y
+func is_rightmost_x(a,b):
+	return a.x>b.x
 
