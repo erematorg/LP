bd6b508e7d709a4d8833c94b3192e4e1a5bf7646
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-10-17 21:54:33 +0200

    Leg improvements

--- LP/systems/movement/Leg.gd
+++ LP/systems/movement/Leg.gd
@@ -12,8 +12,31 @@ class_name Leg
 
 # Onready references for ground detection (e.g., RayCast2D)
 #@onready var ground_raycast: RayCast2D = $GroundRaycast
-@onready var ground_cast : ShapeCast2D = $GroundCast
-
+#@onready var ground_cast : ShapeCast2D = $GroundCast
+@export_category("Rays")
+var ground_rays : Array[RayCast2D] 
+var first_ground_hit : Vector2
+@export var ray_count : int = 6 :
+	set(val):
+		ray_count = clampi(val, 2, 30)
+		call_deferred("ensure_ground_raycast")
+@export var max_angle: float = 180 :
+	set(val):
+		max_angle = val
+		call_deferred("ensure_ground_raycast")
+@export var radius : int = 160 :
+	set(val):
+			radius = val
+			call_deferred("ensure_ground_raycast")
+@export var base_rotation : float = PI / 2 : # Rotation offset in radians, 0 = right, PI/2 = up, PI = left
+	set(val):
+		base_rotation = val
+		call_deferred("ensure_ground_raycast")
+@export var spread_factor = 0.5 :  # Adjust this between 0 (tight) and 1 (full spread)
+	set(val):
+		spread_factor = val
+		call_deferred("ensure_ground_raycast")
+		
 # Signals for ground interaction
 signal touched_ground(leg: Leg)
 signal left_ground(leg: Leg)
@@ -26,37 +49,94 @@ func _ready():
 
 # Ensures that the RayCast2D for ground detection exists
 func ensure_ground_raycast():
-	if !ground_cast:
-		ground_cast = ShapeCast2D.new()
-		ground_cast.name = "GroundCast"
-		add_child(ground_cast, true)
-		ground_cast.set_owner(get_tree().get_edited_scene_root())
-		ground_cast.target_position = (Vector2)(0,80)
-		ground_cast.shape = CapsuleShape2D.new()
-		ground_cast.max_results = 6
+	while ground_rays.size() > ray_count and ground_rays.size() > 0:
+		var ray_to_remove = ground_rays.pop_back()  # Remove the last ray from the array
+		ray_to_remove.queue_free()  # Free it from the scene
+	
+	ground_rays.resize(ray_count)
+	for i in ground_rays.size():
+		if !ground_rays[i]:
+			ground_rays[i] = RayCast2D.new()
+			ground_rays[i].name = "GroundRay_"+str(i)
+			add_child(ground_rays[i])
+		
+		var start_angle = -max_angle / 2 * spread_factor
+		var end_angle = max_angle / 2 * spread_factor
+		var angle = lerp(start_angle, end_angle, float(i) / (ray_count - 1))
+		# Add base rotation to control the hemisphere's orientation
+		var final_angle = angle + base_rotation
+		# Convert the angle to a direction vector (polar to cartesian conversion)
+		var direction = Vector2(cos(final_angle), sin(final_angle))
+		# Set the target position using the calculated direction and ray length (radius)
+		ground_rays[i].target_position = direction * radius
+
 
 # Function to handle walking logic
 func walk(direction: Vector2):
 	if is_grounded:
 		position += direction * walk_speed * get_process_delta_time()
 
+
 # Update function for checking ground status
 func _physics_process(_delta):
-	#ground_raycast.force_raycast_update()
-	ground_cast.force_shapecast_update()
+	for i in ground_rays.size():
+		ground_rays[i].force_raycast_update()
 	check_ground_status()
 	adjust_leg_target()
 
+
 # Checks if the leg is touching the ground
 func check_ground_status():
-	if ground_cast.is_colliding():
+	var closest_distance = INF
+	var closest_ground_hit = null
+	var leg_position = global_position  # The position of the leg or the base point for raycasting
+
+	# Iterate over all the rays and find the closest collision point
+	for i in range(ground_rays.size()):
+		if ground_rays[i].is_colliding():
+			var hit_position = ground_rays[i].get_collision_point()
+			var distance = target_marker.position.distance_to(hit_position) #leg_position.distance_to(hit_position)
+
+			# If this hit is closer than previous hits, store it
+			if distance < closest_distance:
+				closest_distance = distance
+				closest_ground_hit = hit_position
+
+	# If we found a ground hit, update the grounded status and move leg IK to the closest hit
+	if closest_ground_hit != null:
 		if !is_grounded:
 			is_grounded = true
 			emit_signal("touched_ground", self)
+
+		first_ground_hit = closest_ground_hit  # Move the leg IK to this closest hit
 	else:
+		# If no ground is touched, update grounded status
 		if is_grounded:
 			is_grounded = false
 			emit_signal("left_ground", self)
+
+	#for i in ground_rays.size():
+		#if ground_rays[i].is_colliding():
+			##if !is_grounded:
+			#is_grounded = true
+			#emit_signal("touched_ground", self)
+			#first_ground_hit = ground_rays[i].get_collision_point()
+			#return
+#
+	#if is_grounded:
+		#is_grounded = false
+		#emit_signal("left_ground", self)
+		
+	#Old old code
+	#if ground_cast.is_colliding():
+	#	if !is_grounded:
+	#		is_grounded = true
+	#		emit_signal("touched_ground", self)
+	#else:
+	#	if is_grounded:
+	#		is_grounded = false
+	#		emit_signal("left_ground", self)
+
 
 # Adjusts the target marker position to simulate stepping movement
 func adjust_leg_target():
@@ -65,6 +145,6 @@ func adjust_leg_target():
 		#var new_target_position = global_position + Vector2.RIGHT * target_forward_max_reach
 		#new_target_position.y = leg_maximum_height
 		#target_marker.global_position = new_target_position
-	elif is_grounded:
-		target_marker.global_position = ground_cast.get_collision_point(0)
+	elif is_grounded and first_ground_hit != Vector2.ZERO:
+		target_marker.global_position = first_ground_hit
 		#target_marker.global_position = global_position + Vector2.RIGHT * step_height
