43b74e66572d4c3268cddc76d8cea635d4fc49a5
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-10-22 14:25:21 +0200

    socketmove

--- LP/systems/attachment system/Scripts/attachment_socket.gd
+++ LP/systems/attachment system/Scripts/attachment_socket.gd
@@ -3,6 +3,13 @@ extends Node2D
 class_name attachment_socket
 
 var socket_icon = preload("res://systems/attachment system/socket.png")
+var limb : LimbBase
 
 func _ready() -> void:
 	pass # Replace with function body.
+
+func assign_limb():
+	pass
+	
+func remove_limb():
+	pass
