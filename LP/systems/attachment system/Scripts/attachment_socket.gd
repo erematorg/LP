828a8307bc9aecc04d27899e9aafb78553d5bcb0
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-10-26 12:14:32 +0200

    Update attachment_socket.gd

--- LP/systems/attachment system/Scripts/attachment_socket.gd
+++ LP/systems/attachment system/Scripts/attachment_socket.gd
@@ -1,15 +1,32 @@
+@tool
 @icon("res://systems/attachment system/socket.png")
 extends Node2D
-class_name attachment_socket
+class_name AttachmentSocket
+
+var occupied : bool = false
+#A possible way of constraining part types for each socket
+#var accepted_type : int = EntityPart.type.BODY
 
 var socket_icon = preload("res://systems/attachment system/socket.png")
+const GREEN_SOCKET_SMALL = preload("res://addons/attachmentgui/Sprites/green_socket_small.png")
+const RED_SOCKET_SMALL = preload("res://addons/attachmentgui/Sprites/red_socket_small.png")
 var limb : LimbBase
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
-	pass # Replace with function body.
-
-func assign_limb():
-	pass
+	update_sprite()
 	
+func assign_new_limb(part):
+	if part and !occupied:
+		update_sprite()
+		occupied = true
+		part.reparent(get_parent())
+
 func remove_limb():
 	pass
+
+func update_sprite():
+	if occupied:
+		sprite_2d.texture = RED_SOCKET_SMALL
+	else:
+		sprite_2d.texture = GREEN_SOCKET_SMALL
