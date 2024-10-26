92d8f9404ce5cba9e3f2f2e7f5af5ca75f8e8ae5
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-10-26 12:14:27 +0200

    Update entity_part.gd

--- LP/systems/attachment system/Scripts/entity_part.gd
+++ LP/systems/attachment system/Scripts/entity_part.gd
@@ -8,7 +8,7 @@ enum type {BODY, HEAD, APPENDAGE}
 @export var entity_type : type
 var creator : CreatureCreator
 var recently_moved = false
-var closest_socket : attachment_socket
+var closest_socket : AttachmentSocket
 
 
 #Activate notification system
@@ -20,9 +20,19 @@ func inject_creature_creator(cc : CreatureCreator):
 	creator = cc
 
 
-func snap_to_socket(socket : attachment_socket):
+func snap_to_socket(socket : AttachmentSocket):
+	if not socket:
+		push_error("socket is null!")
+		return
 	global_position = socket.global_position
 	recently_moved = false
+	if socket is AttachmentSocket:
+		if socket.has_method("assign_new_limb"):
+			socket.assign_new_limb(self)
+		else:
+			push_warning("socket lacks assign_limb function")
+	else:
+		print("Socket is NOT of type attachment_socket, it is: ", str(socket))
 
 
 #This will trigger whenever the part is moved
