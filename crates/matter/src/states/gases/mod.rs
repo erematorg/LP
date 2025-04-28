a1175db77c1e7f9397014970fcd985b48b8ed192
M1thieu <mathieupierreadrien@gmail.com>
2025-04-29 01:09:29 +0200

    Matter Organization as Modules
    Subcrates -> Modules transition

---
+++ crates/matter/src/states/gases/mod.rs
@@ -0,0 +1,11 @@
+// Currently empty, will be expanded in the future
+
+/// Prelude for the gases module.
+///
+/// This includes components for modeling compressible substances with rapid molecular movement.
+pub mod prelude {
+    // To be populated as gas-related components are implemented
+    // Example future exports:
+    // pub use super::pressure::PressureField;
+    // pub use super::diffusion::DiffusionRate;
+}
